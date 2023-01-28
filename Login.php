509ae82df84757016d064d271c349f1b92f9c236
zoebatz <123593647+zoebatz@users.noreply.github.com>
2023-01-28 15:21:47 -0500

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,56 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error)
+    {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if( $row = $result->fetch_assoc()  )
+        {
+            returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+        }
+        else
+        {
+            returnWithError("No Records Found");
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+?>
