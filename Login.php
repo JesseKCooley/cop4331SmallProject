2ffd3c2005195681cbaf379190148a42c1e81b98
zoebatz <123593647+zoebatz@users.noreply.github.com>
2023-02-02 14:19:18 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -7,7 +7,7 @@
     $lastName = "";
 
     // Create connection
-    $conn = new mysqli(localhost, , $password);
+    $conn = new mysqli("localhost", "root", "welovelamp826", "COP4331");
 
     // Check connection
     if ($conn->connect_error)
