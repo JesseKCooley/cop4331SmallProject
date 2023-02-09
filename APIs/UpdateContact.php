aca34155ddfb91302b9be8fca2b36aad7b15da75
zoebatz <123593647+zoebatz@users.noreply.github.com>
2023-02-09 15:28:27 -0500

    Update UpdateContact.php

--- APIs/UpdateContact.php
+++ APIs/UpdateContact.php
@@ -1,18 +1,5 @@
-<?php
 
-    $inData = getRequestInfo();
-
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDB";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    } 
+  
     
     $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
     
@@ -22,5 +9,70 @@
       echo "Error updating record: " . $conn->error;
     }
     
-    $conn->close();
+<?php
+
+$inData = getRequestInfo();
+
+      
+    $id = $inData["userId"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $number = $inData["number"];
+    $email = $inData["email"];
+   
+    
+    // Create connection
+    $conn = new mysqli("localhost", "root", "?weLOVElamp826", "COP4331");
+
+    // check connection
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+
+        $sql "UPDATE Users SET 
+        FirstName = '$firstName', 
+        LastName = '$lastName', 
+        Number = '$number', 
+        Email = '$email'
+        WHERE ID = = '$id'";
+
+
+        if($conn->query($sql) === TRUE)
+        {
+           echo "Contact information updated successfully.";
+        }
+        else 
+        {
+            echo "Could not update contact information.";
+        }
+
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+  
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
 ?>
