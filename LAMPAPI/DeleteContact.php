49be2f073f528296a63f190d7ddf92a4485beb4b
zoebatz <123593647+zoebatz@users.noreply.github.com>
2023-02-13 20:27:20 -0500

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,27 +1,62 @@
 <?php
 
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDB";
+    $id = $inData["ID"];  
+    $userID = $inData["userID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $number = $inData["number"];
+    $email = $inData["email"];
+   
     
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    } 
-    
-    // sql to delete a record
-    $sql = "DELETE FROM MyGuests WHERE id=3";
-    
-    if ($conn->query($sql) === TRUE) {
-      echo "Record deleted successfully";
-    } else {
-      echo "Error deleting record: " . $conn->error;
+    $conn = new mysqli("localhost", "root", "?weLOVElamp826", "COP4331");
+
+    // check connection
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        // use number to identify contact and userID to make sure deleting from right user
+        $sql = "DELETE FROM Contacts WHERE number = $number AND userID = $userID";  
+
+
+        if($conn->query($sql) === TRUE)
+        {
+           echo "Contact removed.";
+        }
+        else 
+        {
+            echo "Could not remove contact.";
+        }
+
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+  
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
     }
     
-    $conn->close();
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
 ?>
