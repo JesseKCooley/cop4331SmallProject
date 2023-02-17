cd17a1c7a8bd8ee484e9f7a4e664be43ed83d118
JesseKCooley <104393098+JesseKCooley@users.noreply.github.com>
2023-02-17 14:52:51 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "138.197.120.41";
+$host = "138.197.120.41"; //this was once "localhost" and I'm not sure if it's supposed to be that or our server's IP address...?
 $dbname = "cop4331";
 $username = "root";
 $password = "?weLOVElamp826";
