b9fd2e249be28c3a8cbe98461a28bd9b0fbc05bb
Zoe Batz <zoebatz@Zoes-MBP-2.lan>
2023-03-16 19:59:04 -0400

    signup edits

--- new-signup.php
+++ new-signup.php
@@ -28,14 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $formValid = false;
     }
 
-
+}
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
       }
-}
+
 
 ?>
 
