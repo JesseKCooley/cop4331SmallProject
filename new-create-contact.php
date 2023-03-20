9b15c61ad9bd71b1ba2b1f29de03672f4c25da82
Zoe Batz <zoebatz@Zoes-MacBook-Pro-2.local>
2023-03-20 12:02:47 -0400

    edit

--- new-create-contact.php
+++ new-create-contact.php
@@ -19,11 +19,11 @@
             $lasname = test_input($_POST["lastName"]);
         }
 
-        if (! empty($_POST["email"])){
+        if (!(empty($_POST["email"]))){
             $email = test_input($_POST["email"])
         }
 
-        if (! empty($_POST["phoneNumber"])){
+        if (!(empty($_POST["phoneNumber"]))){
             $number = test_input($_POST["phoneNumber"])
         }
 
