2285f24fbe98acf2779e9aa76d67053055dbd911
JesseKCooley <104393098+JesseKCooley@users.noreply.github.com>
2023-03-20 12:46:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Contact Manager</title>
+    <title>Contactr</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
