d0e8128843158f678c4cb1f397e866de34158584
Jordan James <yellowj488@gmail.com>
2023-03-20 16:44:43 -0400

    button edits

--- index.php
+++ index.php
@@ -127,11 +127,11 @@ if (isset($_SESSION["user_id"])) {
     <?php if (isset($user)): ?>
         
     <h4>Hello <?= ($user["userName"]) ?></h4>
-    <div class="btn-text-right">
+    <!--<div class="btn-text-right">
     <a class="btn btn-primary" href="logout.php" role="button">Logout</a>
-    </div>
+    </div> -->
     <p>Below are your contacts.</p>
-    <!--<a style="text-align:right;" class="btn btn-primary" href="logout.php" role="button">Logout</a>-->
+    <a style="btn-text-right;" class="btn btn-primary" href="logout.php" role="button">Logout</a>
     
 
 
