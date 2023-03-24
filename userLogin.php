6d8009247d0936f84c9b5dc428a477aa2a8867a7
Zoe Batz <zoebatz@Zoes-MBP-2.lan>
2023-03-24 14:06:26 -0400

    login

--- userLogin.php
+++ userLogin.php
@@ -15,7 +15,7 @@ if( $conn->connect_error )
 else
 {
     $stmt = $conn->prepare("SELECT * FROM users WHERE userName=?");
-    $stmt->bind_param("s", $inData["email"]);
+    $stmt->bind_param("s", "billybob");
     $stmt->execute();
     $result = $stmt->get_result();
 
